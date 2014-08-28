9791e46dc2346765a9ff3d7e33b1d0be1e0d326f
lvunie <lvunie@gmail.com>
2014-08-28 16:44:44 -0700

    8_27

--- test/example_scraper/example.js
+++ test/example_scraper/example.js
@@ -2,9 +2,13 @@ var scraperjs = require('scraperjs');
 
 //http://www.web-presence-in-china.com/about-us
 //https://news.ycombinator.com/
-scraperjs.StaticScraper.create('https://news.ycombinator.com/')
+
+//var json = { p_tag : }
+
+
+scraperjs.StaticScraper.create('http://www.web-presence-in-china.com/about-us')
     .scrape(function($) {
-        return $(".title a ").map(function() {
+        return $("p").map(function() {
             return $(this).text();
         }).get();
     }, function(news) {
--- test/scraper_v1/server (copy).js
+++ test/scraper_v1/server (copy).js
@@ -4,12 +4,35 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
+var scraperjs = require('scraperjs');
+
+
 
 app.get('/scrape', function(req, res){
+
+
+	var json_wpic = { p_tage : ""};	
+
+	scraperjs.StaticScraper.create('http://www.web-presence-in-china.com/about-us')
+    		.scrape(function($) {
+     		   return $("p").map(function() {
+    		        return $(this).text();
+     		   }).get();
+    		}, function(news) {
+
+		   json_wpic.p_tage = news;
+     		   //console.log(news);
+    		})
+
+  	//fs.writeFile('output_WPIC.json', JSON.stringify(json_wpic, null, 4), function(err){
+        	//console.log('WPIC!!!');
+        //})
+
+
 	
 	url = 'http://www.imdb.com/title/tt1229340/';
-	//url = 'http://www.web-presence-in-china.com/';
         //url = 'http://www.imdb.com/title/tt1355630/?ref_=shtt_ov_tt/';
+
 	request(url, function(error, response, html){
 		if(!error){
 			var $ = cheerio.load(html);
@@ -17,18 +40,12 @@ app.get('/scrape', function(req, res){
 			var title, release, rating;
 			var json = { title : "", release : "", rating : ""};
 
-			//$('#data .h2').each(function() {
-   			//	 console.log($(this).text());
-			//})
-
 			$('.header').filter(function(){
 		        var data = $(this);
-		        title = data.children().first().text();
-			//title = data.text();   
+		        title = data.children().first().text();			
           
                         release = data.children().last().children().text();
-		        //release = data.text();
-
+		   
 		        json.title = title;
                 json.release = release;
 	        })
@@ -46,7 +63,8 @@ app.get('/scrape', function(req, res){
         // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
         // Parameter 3 :  callback function - a callback function to let us know the status of our function
 
-        fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+        //fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+        fs.writeFile('output_WPIC.json', JSON.stringify(json_wpic, null, 4), function(err){
 
         	console.log('File successfully written! - Check your project directory for the output.json file');
 
